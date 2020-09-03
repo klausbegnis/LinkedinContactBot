e014584cc3739323a1274547270f60372cc72721
Klaus Edward Becker Begnis <68127491+klausbegnis@users.noreply.github.com>
2020-09-03 13:59:31 -0300

    Update tabela.py

--- scripts/tabela.py
+++ scripts/tabela.py
@@ -1,8 +1,30 @@
 import xlsxwriter
 import pandas as pd 
+from scripts.sheetsapi import ApiSheets
+
 
 class Excel():
-    def __init__(self, nome):
+    def __init__(self, nome, SCOPES, SPREADSHEET_ID, RANGE_NAME):
+        # Leitura banco de dados
+
+        self.scopes = SCOPES
+        self.spreadsheet_id = SPREADSHEET_ID 
+        self.range_name = RANGE_NAME
+        try:
+            api = ApiSheets(SCOPES=self.scopes, SPREADSHEET_ID=self.spreadsheet_id, RANGE_NAME=self.range_name)
+            data = api.pull_sheet_data()
+            nomes_df = pd.DataFrame(data[1:], columns=data[0])
+            self.nomes_g = nomes_df['Nome'].tolist()
+            self.linkss_g = nomes_df['Linkedin'].tolist()
+            self.telefones_g = nomes_df['Telefone'].tolist()
+            self.emails_g = nomes_df['Email'].tolist()
+            self.enderecos_g = nomes_df['Endereço'].tolist()
+            self.trabalhos_g = nomes_df['Trabalho'].tolist()
+            self.state = 1
+        except:
+            self.state = 2
+            print("Não foi possível abrir o banco de dados, salvaremos sem moficiação")
+        #Leitura da Progress.txt
         self.tab = nome
         self.prgs = pd.read_csv(r"./txts/progress.txt")
         self.nomes = self.prgs['nome'].tolist()
@@ -11,6 +33,7 @@ class Excel():
         self.emails = self.prgs['email'].tolist()
         self.trabalhos = self.prgs['trabalho'].tolist()
         self.enderecos = self.prgs['endereco'].tolist()
+
         self.faz_gráfico()
 
 
@@ -23,7 +46,8 @@ class Excel():
 
         config_titulo = workbook.add_format({'bold': True, 'bg_color': '#C0C0C0'})
         cinza = workbook.add_format({'bg_color': '#C0C0C0'})
-        branco = workbook.add_format({'bold': True, 'bg_color': '#ffffff'})
+        branco = workbook.add_format({'bg_color': '#ffffff'})
+        amarelo = workbook.add_format({'bg_color': '#FFFF00'})
 
         columns = [['Nome', 'A'], ['Linkedin', 'B'], ['Telefone', 'C'], ['Email', 'D'], ['Endereço', 'E'] , ['Trabalho', 'F']]
 
@@ -33,23 +57,130 @@ class Excel():
                 coluna_var = str(coluna[i][0])
                 worksheet.write(f'{coluna_letra}1', f'{coluna_var}', config_titulo)
 
-        def faz_linha(dados, letra):
-            letra_up = str(letra).upper()
-            for i in range(len(dados)):
-                if i == 0:
-                    worksheet.write(f'{letra_up}{i + 2}', f'{dados[i]}', branco)
-                if (i % 2) == 0:
-                    if i != 0:
+        if self.state == 1:
+
+            def faz_linha(dados, letra, tipo):
+                tipo = tipo
+                letra_up = str(letra).upper()
+                for i in range(len(dados)):
+
+                    dado = dados[i]
+                    tg = self.telefones_g[i]
+                    eg = self.emails_g[i]
+                    e_g = self.enderecos_g[i]
+                    t_g = self.trabalhos_g[i]
+
+                    if i == 0:
+                        if tipo != 0:
+
+                            if tipo == 'telefone':
+                                if dado == tg:
+                                    worksheet.write(f'{letra_up}{i + 2}', f'{dado}', branco)
+                                else:
+                                    worksheet.write(f'{letra_up}{i + 2}', f'{dado}', amarelo)
+                            
+                            if tipo == 'email':
+                                if dado == eg:
+                                    worksheet.write(f'{letra_up}{i + 2}', f'{dado}', branco)
+                                else:
+                                    worksheet.write(f'{letra_up}{i + 2}', f'{dado}', amarelo)
+
+                            if tipo == 'endereco':
+                                if dado == e_g:
+                                    worksheet.write(f'{letra_up}{i + 2}', f'{dado}', branco)
+                                else:
+                                    worksheet.write(f'{letra_up}{i + 2}', f'{dado}', amarelo)
+
+                            if tipo == 'trabalho':
+                                if dado == t_g:
+                                    worksheet.write(f'{letra_up}{i + 2}', f'{dado}', branco)
+                                else:
+                                    worksheet.write(f'{letra_up}{i + 2}', f'{dado}', amarelo)
+
+                        else:
+                            worksheet.write(f'{letra_up}{i + 2}', f'{dado}', branco)
+
+                    if (i % 2) == 0:
+                        if i != 0:
+                            if tipo != 0:
+                                
+                                if tipo == 'telefone':
+                                    if dado == tg:
+                                        worksheet.write(f'{letra_up}{i + 2}', f'{dado}', branco)
+                                    else:
+                                        worksheet.write(f'{letra_up}{i + 2}', f'{dado}', amarelo)
+                                
+                                if tipo == 'email':
+                                    if dado == eg:
+                                        worksheet.write(f'{letra_up}{i + 2}', f'{dado}', branco)
+                                    else:
+                                        worksheet.write(f'{letra_up}{i + 2}', f'{dado}', amarelo)
+
+                                if tipo == 'endereco':
+                                    if dado == e_g:
+                                        worksheet.write(f'{letra_up}{i + 2}', f'{dado}', branco)
+                                    else:
+                                        worksheet.write(f'{letra_up}{i + 2}', f'{dado}', amarelo)
+
+                                if tipo == 'trabalho':
+                                    if dado == t_g:
+                                        worksheet.write(f'{letra_up}{i + 2}', f'{dado}', branco)
+                                    else:
+                                        worksheet.write(f'{letra_up}{i + 2}', f'{dado}', amarelo)
+
+                            else:
+                                worksheet.write(f'{letra_up}{i + 2}', f'{dado}', branco)
+                    else:
+                        if tipo != 0:
+
+                            if tipo == 'telefone':
+                                if dado == tg:
+                                    worksheet.write(f'{letra_up}{i + 2}', f'{dado}', cinza)
+                                else:
+                                    worksheet.write(f'{letra_up}{i + 2}', f'{dado}', amarelo)
+                                
+                            if tipo == 'email':
+                                if dado == eg:
+                                    worksheet.write(f'{letra_up}{i + 2}', f'{dado}', cinza)
+                                else:
+                                    worksheet.write(f'{letra_up}{i + 2}', f'{dado}', amarelo)
+
+                            if tipo == 'endereco':
+                                if dado == e_g:
+                                    worksheet.write(f'{letra_up}{i + 2}', f'{dado}', cinza)
+                                else:
+                                    worksheet.write(f'{letra_up}{i + 2}', f'{dado}', amarelo)
+
+                            if tipo == 'trabalho':
+                                if dado == t_g:
+                                    worksheet.write(f'{letra_up}{i + 2}', f'{dado}', cinza)
+                                else:
+                                    worksheet.write(f'{letra_up}{i + 2}', f'{dado}', amarelo)
+
+                        else:
+                            worksheet.write(f'{letra_up}{i + 2}', f'{dado}', cinza)
+        else:
+            def faz_linha(dados, letra, tipo):
+                tipo = tipo
+                letra_up = str(letra).upper()
+                for i in range(len(dados)):
+                    if i == 0:
                         worksheet.write(f'{letra_up}{i + 2}', f'{dados[i]}', branco)
-                else:
-                    worksheet.write(f'{letra_up}{i + 2}', f'{dados[i]}', cinza)
-        
+                    if (i % 2) == 0:
+                        if i != 0:
+                            worksheet.write(f'{letra_up}{i + 2}', f'{dados[i]}', branco)
+                    else:
+                        worksheet.write(f'{letra_up}{i + 2}', f'{dados[i]}', cinza)
+
+
         faz_coluna(columns)
-        faz_linha(self.nomes, 'a')
-        faz_linha(self.links, 'b')
-        faz_linha(self.telefones, 'c')
-        faz_linha(self.emails, 'd')
-        faz_linha(self.enderecos, 'e')
-        faz_linha(self.trabalhos, 'f')
+        faz_linha(self.nomes, 'a', 0)
+        faz_linha(self.links, 'b', 0)
+        faz_linha(self.telefones, 'c', 'telefone')
+        faz_linha(self.emails, 'd', 'email')
+        faz_linha(self.enderecos, 'e', 'endereco')
+        faz_linha(self.trabalhos, 'f', 'trabalho')
 
         workbook.close()
+
+        
