77ff310d29eb76f36c3d959dd2363e2281e0a2fb
Klaus Edward Becker Begnis <68127491+klausbegnis@users.noreply.github.com>
2020-09-16 18:33:08 -0300

    Update linkedinclass.py

--- scripts/linkedinclass.py
+++ scripts/linkedinclass.py
@@ -43,7 +43,7 @@ class LinkedinBot():
             print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
             # Print New Line on Complete
             if iteration == total: 
-                print()
+               print()
         for i in range(len(links)):
             l = len(links)
             printProgressBar(i + 1 , l, prefix = 'Progress:', suffix = 'Complete', length = 50)
@@ -74,7 +74,7 @@ class LinkedinBot():
                         pass
                 sleep(1)
 
-                #BLOCO DE INFORMAÇÕES DE CONTATO
+                    #BLOCO DE INFORMAÇÕES DE CONTATO
                 if self.driver.find_element_by_xpath('//header[@class = "pv-contact-info__header t-16 t-black t-bold"]'):
                     """ #RETIRA LINK DO PERFIL
 
@@ -82,9 +82,9 @@ class LinkedinBot():
                         section_link = self.driver.find_element_by_xpath('//section[@class = "pv-contact-info__contact-type ci-vanity-url"]')
                         link_ln1 = section_link.find_element_by_xpath('//a[@class = "pv-contact-info__contact-link link-without-visited-state t-14"]')
                         link_ln = link_ln1.text """
-        
-                        
-                    #RETIRA NÚMERO DO CELULAR (SE INFORMADO)
+            
+                            
+                        #RETIRA NÚMERO DO CELULAR (SE INFORMADO)
                     try:
                         if self.driver.find_element_by_xpath('//section[@class = "pv-contact-info__contact-type ci-phone"]'):
                             section_phone =  self.driver.find_element_by_xpath('//section[@class = "pv-contact-info__contact-type ci-phone"]')
@@ -109,15 +109,46 @@ class LinkedinBot():
                     self.driver.find_element_by_xpath('//button[@class = "artdeco-modal__dismiss artdeco-button artdeco-button--circle artdeco-button--muted artdeco-button--2 artdeco-button--tertiary ember-view"]').click()
                     sleep(1)
                     # PEGA EXPERIÊNCIA TOP 1
+                    
+                    # load da pagina toda
+                    html = self.driver.find_element_by_tag_name('html')
+                    for _ in range(4):
+                        html.send_keys(Keys.PAGE_DOWN)
 
-                    experience_list = self.driver.find_element_by_xpath('//a[@class = "pv-top-card--experience-list-item"]')
+                    sectionExperiencia = self.driver.find_elements_by_xpath('//section[@id = "experience-section"]/ul//li')
+                    secao1 = sectionExperiencia[0]
+
+                    """ tag_p = secao1.find_elements_by_tag_name('p')
+                    for elements in tag_p:
+                        print(elements.text) """
+                    sState = 'var'
+                    try:
+                        tag_s = secao1.find_elements_by_tag_name('span')
+                        for s in range(len(tag_s)):
+                            if tag_s[s].text == "Nome da empresa":
+                                trabalho = tag_s[s+1].text
+                                sState = 0
+                                s = len(tag_s) + 10
+                    except:
+                        pass
+                    if sState != 0:
+                        try:
+                            tag_p = secao1.find_elements_by_tag_name('p')
+                            for p in range(len(tag_p)):
+                                if tag_p[p].text == "Nome da empresa":
+                                    trabalho = tag_p[p+1].text
+                                    p = len(tag_p) + 10
+                        except:
+                            pass
+                    
+                    """ experience_list = self.driver.find_element_by_xpath('//a[@class = "pv-top-card--experience-list-item"]')
                     experiencia = [experience_list.find_element_by_xpath('//span[@class = "text-align-left ml2 t-14 t-black t-bold full-width lt-line-clamp lt-line-clamp--multi-line ember-view"]')]
-                    trabalho = experiencia[0].text
+                    trabalho = experiencia[0].text """
+
                     pgrs = pgrs.append({"nome": nome[i],"linkedin" : links[i], "telefone" : phone_number, "email" : email, "trabalho" : trabalho, "endereco" : endereco}, ignore_index=True)
                     pgrs.to_csv(r"./txts/progress.txt", header=True, index=False, sep=",")
-
-                sleep(2)
-                i += 1
+                    sleep(2)
+                    i += 1
             except:
                 print("Algum elemento ou perfil não foi encontrado... Continuando")
                 pgrs = pgrs.append({"nome": nome[i], "linkedin": links[i], "telefone": "NOME", "email" : "NAO", "trabalho" :  "ENCONTRADO", "endereco" : endereco}, ignore_index=True)
